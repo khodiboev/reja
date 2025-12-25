bea00e8e6df2aa8b27f804af250e8e2fd0b6b191
Victor <khadibayevjurabek@gmail.com>
2025-12-25 12:47:05 +0900

    feat: develop author routher ejs page

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log('Web Serverni boshlash');
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
 
 //1: Kirish kodlari
 app.use(express.static("public"));
@@ -20,9 +31,14 @@ app.post("/create-item", (req, res) => {
     res.json({test: "Success"});
 })
 
+app.get("/author", (req, res) => {
+    res.render('author', {user: user});
+})
+
 app.get('/', function(req, res) {
     res.render('harid');
 })
+
 
 const server = http.createServer(app);
 let PORT = 3000;
