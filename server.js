c5ce5560092a3491b69c7ddea750053c91e3c655
Victor <khadibayevjurabek@gmail.com>
2025-12-25 11:33:52 +0900

    Add views templates and update server

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4: Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1">Play Maker who is Victor</h1>`);
-});
-app.get("/gift", function(req, res) {
-    res.end(`<h1">You are in gift section</h1>`);
-});
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "Success"});
+})
+
+app.get('/', function(req, res) {
+    res.render('harid');
+})
 
 const server = http.createServer(app);
 let PORT = 3000;
